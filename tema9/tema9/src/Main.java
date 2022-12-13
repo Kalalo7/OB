22fb3fad9852c78944675aa5990a8df532e8d5fc
Kalalo7 <martinfauci@gmail.com>
2022-12-13 19:39:46 +0100

    completado ejercicio 9

--- tema9/tema9/src/Main.java
+++ tema9/tema9/src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //creo la clase persona
+        class Persona {
+            String nombre;
+            int edad;
+            long telefono;
+        }
+        // creo clase cliente
+        class Cliente extends Persona {
+            double credito;
+        }
+        class Trabajador extends Persona {
+            double salario;
+        }
+
+        Cliente cliente = new Cliente();
+        Trabajador trabajador = new Trabajador();
+
+        //creo objeto de la clase cliente con las propiedades solicitadas
+        cliente.nombre = "Martin";
+        cliente.edad = 32;
+        cliente.telefono = 2215480321L;
+        cliente.credito = 369.45;
+        System.out.println("Hola, soy " + cliente.nombre + ", tengo " + cliente.edad + " años de edad y mi numero de telefono es "+ cliente.telefono + ". Mi credito disponible es €" + cliente.credito + " euros.");
+
+        //creo el objeto de la clase trabajador
+        trabajador.nombre = "Juan Carlos";
+        trabajador.edad = 35;
+        trabajador.telefono = 689051847L;
+        trabajador.salario = 2500;
+        System.out.println("Hola, soy " + trabajador.nombre + ",tengo " + trabajador.edad + " años de edad y mi numero de telefono es " + trabajador.telefono + ". Mi Salario es de € " + trabajador.salario + " euros.");
     }
 }
